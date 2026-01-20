7fe4f137c967d50b5b2544c6f9c1c6ae85f1f2b8
Jarizapulido <javierarizaxd@gmail.com>
2026-01-20 11:17:25 +0100

    Añadimos nuevas funciones

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import { initDB } from "./db/init";
-import { getAllMovies } from "./models/movies";
+import { getAllMovies, getMoviesById, getMoviesByTitle} from "./models/movies";
 
 const db = await initDB()
 //const peliculas = getAllMovies(db)
 //console.log(peliculas)
+//const pelicula = getMoviesById(db, 1234)
+///console.log(pelicula)
+const pelicula = getMoviesByTitle(db, "")
+console.log(pelicula)
+
--- models/movies.ts
+++ models/movies.ts
@@ -4,3 +4,13 @@ export function getAllMovies(db:Database){
     const query=db.query("SELECT * FROM movies")
     return query.all()
 }
+
+export function getMoviesById(db:Database, id: number){
+    const query=db.query("SELECT * FROM movies WHERE id = ?")
+    return query.get(id)
+}
+
+export function getMoviesByTitle(db:Database, nombre: string){
+    const query=db.query("SELECT * FROM movies WHERE nombre like ? ")
+    return query.all(`%${nombre}%`)
+}
