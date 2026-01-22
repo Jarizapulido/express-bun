b59fb2b67e2e8a9384061794491ef9fe455f5c02
Jarizapulido <javierarizaxd@gmail.com>
2026-01-22 10:16:56 +0100

    get /movies ahora acepta parametros en la query

--- index.ts
+++ index.ts
@@ -1,14 +1,36 @@
 import { initDB } from "./db/init";
 import { getAllMovies, getMoviesById, getMoviesByTitle} from "./models/movies";
 import express from "express"
+import type {Request, Response, NextFunction} from "express"
 
 const app = express();
 const PORT = 3000;
 const db = await initDB()
 
+const logMiddleware = (req: Request, res: Response, next: NextFunction) => {
+    //codigo
+    console.log(req.method, req.url, new Date().toISOString())
+
+    if(req.method === "GET"){
+        console.log(req.query)
+    }
+    
+    next()
+}
+
+app.use(logMiddleware)
+
 app.get("/movies", (req,res) => {
-    const peliculas = getAllMovies(db)
-    res.json(peliculas)
+    const title = req.query.title
+    if(title){
+        const peliculas = getMoviesByTitle(db,title)
+        res.json(peliculas)
+
+    }
+    else{
+        const peliculas = getAllMovies(db)
+        res.json(peliculas)
+    }
 })
 
 app.listen(PORT, () =>{
