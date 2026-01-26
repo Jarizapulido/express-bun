ccea814056774befd78336dc50678926067ce035
Jarizapulido <javierarizaxd@gmail.com>
2026-01-26 10:39:59 +0100

    creamos tabla html

--- index.ts
+++ index.ts
@@ -18,6 +18,7 @@ const logMiddleware = (req: Request, res: Response, next: NextFunction) => {
     next()
 }
 
+app.use(express.static("public"))
 app.use(logMiddleware)
 
 app.get("/movies", (req,res) => {
---
+++ public/main.js
@@ -0,0 +1,29 @@
+// fetch('/movies')
+// .then(response => response.json())
+// .then(data =>{
+//     console.log(data);
+// })
+
+const respuesta = await fetch("/movies")
+const movies = await respuesta.json()
+console.log(movies)
+
+const tableBody = document.querySelector("#tabla")
+console.log(tableBody)
+
+movies.forEach(movie => {
+    const tr = document.createElement("tr")
+    const tdId = document.createElement("td")
+    const tdTitle = document.createElement("td")
+    const tdGenres = document.createElement("td")
+    
+    tdId.textContent = movie.id
+    tdTitle.textContent = movie.nombre
+    tdGenres.textContent = movie.genre.replaceAll("|", ", ")
+
+    tr.appendChild(tdId)
+    tr.appendChild(tdTitle)
+    tr.appendChild(tdGenres)
+    tableBody.appendChild(tr)
+
+});
